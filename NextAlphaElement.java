7ae672a2ea2846a6a4d55688e7023a534b873f93
Sahana Chakravarty <58138334+sahana0007@users.noreply.github.com>
2021-07-21 13:19:47 +0530

    Update NextAlphaElement.java

--- NextAlphaElement.java
+++ NextAlphaElement.java
@@ -1,50 +1,75 @@
 package com.datastructure;
 
-    public class NextAlphaElement
+import java.util.Scanner;
+
+public class NextAlphaElement
+{
+
+    /* Returns the smallest character from the given
+    set of letters that is greater than target */
+    static char nextGreatestAlphabet(char[] array,
+                                     char target)
     {
+        int n = array.length;
+        if(target>=array[n-1])
+            return array[0];
 
-        /* Returns the smallest character from the given
-        set of letters that is greater than K */
-        static char nextGreatestAlphabet(char alphabets[],
-                                         char K)
+        int l = 0, r = array.length - 1;
+
+        int ans = -1;
+        // Take the first element as l and
+        // the rightmost element as r
+        while (l <= r)
         {
-            int n = alphabets.length;
-            if(K>=alphabets[n-1])
-                return alphabets[0];
-
-            int l = 0, r = alphabets.length - 1;
-
-            int ans = -1;
-            // Take the first element as l and
-            // the rightmost element as r
-            while (l <= r)
+            // if this while condition does not
+            // satisfy simply return the first
+            // element.
+            int mid = (l + r) / 2;
+            if (array[mid] > target)
             {
-                // if this while condition does not
-                // satisfy simply return the first
-                // element.
-                int mid = (l + r) / 2;
-                if (alphabets[mid] > K)
-                {
-                    r = mid - 1;
-                    ans = mid;
-                }
-                else
-                    l = mid + 1;
+                r = mid - 1;
+                ans = mid;
             }
-
-            // Return the smallest element
-            return alphabets[ans];
+            else
+                l = mid + 1;
         }
 
-        // Driver Code
-        public static void main(String[] args)
-        {
-            char letters[] = { 'A', 'r', 'z' };
-            char K = 'z';
-            char result = nextGreatestAlphabet(letters, K);
-
-            // Function call
-            System.out.println(result);
-        }
+        // Return the smallest element
+        return array[ans];
     }
 
+    // Driver Code
+    public static void main(String[] args)
+    {
+        /*
+        char letters[] = { 'A', 'r', 'z' };
+        char K = 'z';
+        char result = nextGreatestAlphabet(letters, K);
+
+         */
+
+        // Function call
+//          System.out.println(result);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("First add some characters...");
+        char[] array = sc.nextLine().replace(" ", "").toCharArray(); 
+        for (int i = 0; i < array.length; i++)
+            System.out.println(array[i]);
+        /*
+        System.out.println("\nYou have entered: ");  //for-each loop to print the string
+        for(String str: string)
+        {
+            System.out.println(str);
+        }
+
+         */
+
+        System.out.println("Enter the number to search:");
+        char target = sc.next().charAt(0);
+        System.out.println(target);
+        char ans = nextGreatestAlphabet(array,target);
+
+        // Function call
+        System.out.println("The next element is " +ans);
+    }
+}
